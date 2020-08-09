2157c82aee5531a4d7e8da59bee5662684c8556c
Lewis Smith <lwsmith98@gmail.com>
2020-08-09 11:31:21 -0500

    Modulize register

--- src/mips/hardware/register.rs
+++ src/mips/hardware/register.rs
@@ -16,112 +16,114 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/// Register Object
-///
-/// Consists of:
-/// * Register Name
-/// * Numeric Register Identifier
-/// * Current Register Value
-/// * Register Reset Value
-struct Register {
-    name: String,
-    number: i32,
-    value: i32,
-    reset_value: i32,
-}
-
-
-/// Register Constructor
-///
-/// # Usage
-///
-/// ```
-/// let mut new_register: Register = make_register("$s0", 16, 0, 0);
-/// ```
-fn make_register(reg_name: &String, reg_id: i32, reg_val: i32, reset_val: i32) -> Register {
-    Register {
-        name: reg_name,
-        number: reg_id,
-        value: reg_val,
-        reset_value: reset_val,
+mod Register {
+    /// Register Object
+    ///
+    /// Consists of:
+    /// * Register Name
+    /// * Numeric Register Identifier
+    /// * Current Register Value
+    /// * Register Reset Value
+    struct Register {
+        name: String,
+        number: i32,
+        value: i32,
+        reset_value: i32,
     }
-}
 
-/// Register Instance Methods
-impl Register {
-    /// Returns a register assembly name
+    /// Register Constructor
     ///
     /// # Usage
     ///
     /// ```
-    /// let reg_name: String = new_register.get_name();
-    /// ````
-    fn get_name(&self) -> String {
-        self.name.clone()
+    /// let mut new_register: Register = make_register("$s0", 16, 0, 0);
+    /// ```
+    fn make_register(reg_name: &String, reg_id: i32, reg_val: i32, reset_val: i32) -> Register {
+        Register {
+            name: reg_name,
+            number: reg_id,
+            value: reg_val,
+            reset_value: reset_val,
+        }
     }
 
-    /// Returns a register's numeric ID
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// let reg_id: i32 = new_register.get_id();
-    /// ```
-    fn get_id(&self) -> i32 {
-        self.number
-    }
+    /// Register Instance Methods
+    impl Register {
+        /// Returns a register assembly name
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// let reg_name: String = new_register.get_name();
+        /// ````
+        pub fn get_name(&self) -> String {
+            self.name.clone()
+        }
 
-    /// Returns the value a register contains
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// let reg_value = new_register.get_value();
-    /// ```
-    fn get_value(&self) -> i32 {
-        self.value
-    }
+        /// Returns a register's numeric ID
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// let reg_id: i32 = new_register.get_id();
+        /// ```
+        pub fn get_id(&self) -> i32 {
+            self.number
+        }
 
-    /// Returns the default value of a register
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// let reset_val = new_register.get_resetval();
-    /// ```
-    fn get_resetval(&self) -> i32 {
-        self.reset_value
-    }
+        /// Returns the value a register contains
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// let reg_value = new_register.get_value();
+        /// ```
+        pub fn get_value(&self) -> i32 {
+            self.value
+        }
 
-    /// Sets the value of a register
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// new_register.set_value(1234567);
-    /// ```
-    fn set_value(&mut self, new_val: i32) -> None {
-        self.value = new_val
-    }
+        /// Returns the default value of a register
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// let reset_val = new_register.get_resetval();
+        /// ```
+        pub fn get_resetval(&self) -> i32 {
+            self.reset_value
+        }
 
-    /// Sets the default value of a register
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// new_register.set_resetval(1);
-    /// ```
-    fn set_resetval(&mut self, new_val: i32) -> None {
-        self.reset_va;ie = new_val;
-    }
+        /// Sets the value of a register
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// new_register.set_value(1234567);
+        /// ```
+        pub fn set_value(&mut self, new_val: i32) -> None {
+            self.value = new_val
+        }
 
-    /// Resets the register value to its default value
-    ///
-    /// # Usage
-    ///
-    /// ```
-    /// new_register.reset_register();
-    fn reset_register(&mut self) -> None {
-        self.value = self.reset_value;
+        /// Sets the default value of a register
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// new_register.set_resetval(1);
+        /// ```
+        pub fn set_resetval(&mut self, new_val: i32) -> None {
+            self.reset_va;
+            ie = new_val;
+        }
+
+        /// Resets the register value to its default value
+        ///
+        /// # Usage
+        ///
+        /// ```
+        /// new_register.reset_register();
+        pub fn reset_register(&mut self) -> None {
+            self.value = self.reset_value;
+        }
     }
 }
